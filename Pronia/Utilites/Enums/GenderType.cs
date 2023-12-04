ab151abdef7fafbc43ae3705944b228e4d1407e4
MuradGanbarov <tu6xfxlrk@code.edu.az>
2023-12-05 03:39:02 +0400

    regex added gender is fixed

--- Pronia/Utilites/Enums/GenderType.cs
+++ Pronia/Utilites/Enums/GenderType.cs
@@ -5,7 +5,7 @@ namespace Pronia.Utilites.Enums
     public enum GenderType
     {
         [Display(Name = "Male")] Male = 1,
-        [Display(Name = "Female")] Female = 2,
+        [Display(Name = "Female")] Female = 2
     }
    
 }
